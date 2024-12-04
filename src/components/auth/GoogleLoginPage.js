2c72caf3ae7832610100cc18000463c4127f1d2b
Xalil404 <kareemxalil@gmail.com>
2024-12-04 15:11:01 +0000

    Made google login a redirect instead of a popup

--- src/components/auth/GoogleLoginPage.js
+++ src/components/auth/GoogleLoginPage.js
@@ -42,12 +42,21 @@ const GoogleLoginPage = () => {
         <GoogleOAuthProvider clientId="26271032790-djnijd5ookmvg0d58pneg2l8l6bdgvbn.apps.googleusercontent.com">
             <div>
                 <h1>Google Login</h1>
+                {/*
                 <GoogleLogin
                     onSuccess={responseGoogle}
                     onError={() => console.error('Login Failed')}
                     ux_mode="popup"
                     cookiePolicy="single_host_origin"
                 />
+                */}
+                <GoogleLogin
+                    ux_mode="redirect"
+                    redirectUri="https://web-frontend-dun.vercel.app/auth/callback"
+                    onSuccess={responseGoogle}
+                    onError={() => console.error('Login Failed')}
+                />
+
             </div>
         </GoogleOAuthProvider>
     );
